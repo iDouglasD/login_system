5875d75f1e6b35e6014cb2cb99a604ecb114ce8c
Douglas Duarte <douglasduarte.eng@gmail.com>
2022-02-04 18:09:53 -0300

    Routes and ProtectedLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,25 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from "react";
+import { AuthProvider } from "./context/AuthProvider";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ProtectedLayout } from "./components/ProtectedLayout";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    return (
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="/profile"
+                        element={
+                            <ProtectedLayout>
+                                <h2>Olá esse é o componente profile!</h2>
+                            </ProtectedLayout>
+                        }
+                    />
+                    <Route path="/login" />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
+    );
 }
-
-export default App
+export default App;
